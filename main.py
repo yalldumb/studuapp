b7882bba7920a4bf223f5cd326d4aa607913aa74
xrttkl <xrttkl@xrttkls-MacBook-Air.local>
2025-11-27 20:11:36 -0800

    Обновляем main.py и мини-приложение с улучшенной графикой

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import WebAppInfo
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import logging
 
-# ---- Новый токен Telegram ----
+# ---- Токен Telegram ----
 BOT_TOKEN = "8560279215:AAFUL0g6bfqYYdEqf95Hi7p6iA-C5zS4ACU"
 
+# ---- Настройка бота ----
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
+# ---- /start обработчик ----
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     webapp_button = types.KeyboardButton(
         text="Открыть мини-приложение 📚",
-        web_app=WebAppInfo(url="http://127.0.0.1:8080/index.html")
+        web_app=WebAppInfo(url="https://yalldumb.github.io/studuapp/")
     )
     keyboard.add(webapp_button)
     await message.answer("Нажми кнопку, чтобы открыть мини-приложение 👇", reply_markup=keyboard)
 
+# ---- Запуск бота ----
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
